1fd50133fce53391bf8041f8bcb7509c9e21b023
yuhan6665 <yuhan6665@users.noreply.github.com>
2024-11-01 14:28:59 +0000

    deploy: XTLS/Xray-docs-next@7e247810849872763ec7c20c545819ad88d33c05

---
+++ assets/classDiagram-FEGYTUDG-BMYbhB6k.js
@@ -0,0 +1,1 @@
+import{c as s,a as r,b as e,s as l}from"./chunk-CXDZ2C6O-D-d6_dj3.js";import{_ as t}from"./mermaid.core-BS5DkABB.js";import"./chunk-EICJXIV7-DrYub5dt.js";import"./app-cRt0-EsY.js";var _={parser:s,db:r,renderer:e,styles:l,init:t(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute,r.clear()},"init")};export{_ as diagram};
