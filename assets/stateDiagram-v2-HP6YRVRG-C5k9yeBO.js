fb72fbd9b276174dbe5200edf9ae7bf94c75b579
Fangliding <Fangliding@users.noreply.github.com>
2025-01-18 07:49:44 +0000

    deploy: XTLS/Xray-docs-next@fda58089da571455643e22fbb9b251568c81ed92

--- assets/stateDiagram-v2-HP6YRVRG-C5k9yeBO.js
+++
@@ -1,1 +0,0 @@
-import{s as t,a as e,c as r,b as s}from"./chunk-JZAHL7AJ-DhkMy2R5.js";import{_ as i}from"./mermaid.core-l9H8koMs.js";import"./chunk-EICJXIV7-BeYRuMBo.js";import"./app-C4LqmcBe.js";var _={parser:t,db:e,renderer:r,styles:s,init:i(a=>{a.state||(a.state={}),a.state.arrowMarkerAbsolute=a.arrowMarkerAbsolute,e.clear()},"init")};export{_ as diagram};
